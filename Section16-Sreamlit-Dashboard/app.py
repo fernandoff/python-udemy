8584de1e8ea3a523b147e5511f756153c1853eed
fernando <fernando.cua@gmail.com>
2025-12-15 16:33:35 -0300

    ajuste ref import

--- Section16-Sreamlit-Dashboard/app.py
+++ Section16-Sreamlit-Dashboard/app.py
@@ -2,7 +2,8 @@ import streamlit as st
 import plotly.express as px
 from dataset import df
 from utils import format_number
-from graficos import grafico_map_estado, grafico_rec_mensal, grafico_rec_estado, grafico_rec_categoria, grafico_rec_vendedores, grafico_vendas_vendedores
+#from graficos import grafico_map_estado, grafico_rec_mensal, grafico_rec_estado, grafico_rec_categoria, grafico_rec_vendedores, grafico_vendas_vendedores
+import graficos
 
 st.set_page_config(layout='wide')
 st.title("Dashboard de Vendas :shopping_trolley:")
@@ -24,16 +25,16 @@ with aba2:
     coluna1, coluna2 = st.columns(2)
     with coluna1:
         st.metric('Receita Total', format_number(df['Preço'].sum(), 'R$'))
-        st.plotly_chart(grafico_map_estado, use_container_width=True)
-        st.plotly_chart(grafico_rec_estado, use_container_width=True)
+        st.plotly_chart(graficos.grafico_map_estado, use_container_width=True)
+        st.plotly_chart(graficos.grafico_rec_estado, use_container_width=True)
     with coluna2:
         st.metric('Quantidade de Vendas', format_number(df.shape[0]))
-        st.plotly_chart(grafico_rec_mensal, use_container_width=True)
-        st.plotly_chart(grafico_rec_categoria, use_container_width=True)
+        st.plotly_chart(graficos.grafico_rec_mensal, use_container_width=True)
+        st.plotly_chart(graficos.grafico_rec_categoria, use_container_width=True)
 with aba3:
     coluna1, coluna2 = st.columns(2)
     with coluna1:
-        st.plotly_chart(grafico_rec_vendedores)
+        st.plotly_chart(graficos.grafico_rec_vendedores)
     with coluna2:
-        st.plotly_chart(grafico_vendas_vendedores)
+        st.plotly_chart(graficos.grafico_vendas_vendedores)
     
